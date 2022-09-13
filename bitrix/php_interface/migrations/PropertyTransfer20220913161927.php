b47b94c12c24c545e8577ebddea25cf13c434f88
Sadovin Vladislav <sadovin.vladislav@gmail.com>
2022-09-13 16:38:12 +0300

    Created migration for new property

---
+++ bitrix/php_interface/migrations/PropertyTransfer20220913161927.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class PropertyTransfer20220913161927 extends Version
+{
+    protected $description = "Миграция нового свойства инфоблока \"Новости\"";
+
+    protected $moduleVersion = "4.1.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $iblockId = $helper->Iblock()->getIblockIdIfExists('furniture_news_s1', 'news');
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Пользовательское свойство',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'custom_property',
+  'DEFAULT_VALUE' => 
+  array (
+    'TYPE' => 'HTML',
+    'TEXT' => '',
+  ),
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'Y',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '1',
+  'USER_TYPE' => 'HTML',
+  'USER_TYPE_SETTINGS' => 
+  array (
+    'height' => 200,
+  ),
+  'HINT' => '',
+  'FEATURES' => 
+  array (
+    0 => 
+    array (
+      'MODULE_ID' => 'iblock',
+      'FEATURE_ID' => 'DETAIL_PAGE_SHOW',
+      'IS_ENABLED' => 'Y',
+    ),
+    1 => 
+    array (
+      'MODULE_ID' => 'iblock',
+      'FEATURE_ID' => 'LIST_PAGE_SHOW',
+      'IS_ENABLED' => 'Y',
+    ),
+  ),
+));
+    
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
